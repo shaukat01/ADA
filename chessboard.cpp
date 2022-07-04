cbb0e38341cd2c2f9d51324922deb7af3705e537
Md Shaukat <86542840+shaukat01@users.noreply.github.com>
2022-07-04 11:59:16 +0530

    Update chessboard.cpp

--- chessboard.cpp
+++ chessboard.cpp
@@ -1,54 +1,92 @@
-#include<bits/stdc++.h>
-#include<iostream>
+// C++ program to place tiles
+#include <bits/stdc++.h>
 using namespace std;
-int board[20][20];
-int tile;
-void tiling(int trow, int tcol, int drow, int dcol,int size){
-    if(size==1)
-        return;
-    int qsize=size/2;
-    int tileToUse=tile++;
-    if(drow<=trow+qsize-1 && dcol<=tcol+qsize-1){
-        tiling(trow,tcol,drow,dcol,qsize);
-    }
-    else{
-        board[trow+qsize-1][tcol+qsize-1]=tileToUse;
-        tiling(trow,tcol,trow+qsize-1,tcol+qsize-1,qsize);
-    }
-    if(drow<=trow+qsize-1 && dcol>=tcol+qsize){
-        tiling(trow,tcol+qsize,drow,dcol,qsize);
-    }
-     else{
-        board[trow+qsize-1][tcol+qsize]=tileToUse;
-        tiling(trow,tcol+qsize,trow+qsize-1,tcol+qsize,qsize);
-    }
-    if(drow>=trow+qsize && dcol<=tcol+qsize-1){
-        tiling(trow+qsize,tcol,drow,dcol,qsize);
-    }
-    else{
-        board[trow+qsize][tcol+qsize-1]=tileToUse;
-        tiling(trow+qsize,tcol,trow+qsize,tcol+qsize-1,qsize);
-    }
-    if(drow>=trow+qsize && dcol>=tcol+qsize){
-        tiling(trow+qsize,tcol+qsize,drow,dcol,qsize);
-    }
-    else{
-        board[trow+qsize][tcol+qsize]=tileToUse;
-        tiling(trow+qsize,tcol+qsize,trow+qsize,tcol+qsize,qsize);
-    }
-}
 
-int main(){
-    int n,drow,dcol,i,j;
-    cout<<"Enter the size"<<endl;
-    cin>>n;
-    cout<<"Enter defective row and column"<<endl;
-    cin>>drow>>dcol;
-    tiling(0,0,drow,dcol,n);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cout<<board[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+int size_of_grid, b, a, cnt = 0;
+int arr[128][128];
+
+// Placing tile at the given coordinates
+void place(int x1, int y1, int x2,
+		int y2, int x3, int y3)
+{
+	cnt++;
+	arr[x1][y1] = cnt;
+	arr[x2][y2] = cnt;
+	arr[x3][y3] = cnt;
+}
+// Quadrant names
+// 1 2
+// 3 4
+
+// Function based on divide and conquer
+int tile(int n, int x, int y)
+{
+	int r, c;
+	if (n == 2) {
+		cnt++;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[x + i][y + j] == 0) {
+					arr[x + i][y + j] = cnt;
+				}
+			}
+		}
+		return 0;
+	}
+	// finding hole location
+	for (int i = x; i < x + n; i++) {
+		for (int j = y; j < y + n; j++) {
+			if (arr[i][j] != 0)
+				r = i, c = j;
+		}
+	}
+
+	// If missing tile is 1st quadrant
+	if (r < x + n / 2 && c < y + n / 2)
+		place(x + n / 2, y + (n / 2) - 1, x + n / 2,
+			y + n / 2, x + n / 2 - 1, y + n / 2);
+
+	// If missing Tile is in 3rd quadrant
+	else if (r >= x + n / 2 && c < y + n / 2)
+		place(x + (n / 2) - 1, y + (n / 2), x + (n / 2),
+			y + n / 2, x + (n / 2) - 1, y + (n / 2) - 1);
+
+	// If missing Tile is in 2nd quadrant
+	else if (r < x + n / 2 && c >= y + n / 2)
+		place(x + n / 2, y + (n / 2) - 1, x + n / 2,
+			y + n / 2, x + n / 2 - 1, y + n / 2 - 1);
+
+	// If missing Tile is in 4th quadrant
+	else if (r >= x + n / 2 && c >= y + n / 2)
+		place(x + (n / 2) - 1, y + (n / 2), x + (n / 2),
+			y + (n / 2) - 1, x + (n / 2) - 1,
+			y + (n / 2) - 1);
+
+	// dividing it again in 4 quadrants
+	tile(n / 2, x, y + n / 2);
+	tile(n / 2, x, y);
+	tile(n / 2, x + n / 2, y);
+	tile(n / 2, x + n / 2, y + n / 2);
+
+	return 0;
+}
+// Driver program to test above function
+int main()
+{
+	// size of box
+	cout<<"Enter the size"<<endl;
+	cin>>size_of_grid;
+	memset(arr, 0, sizeof(arr));
+	// Coordinates which will be marked
+	cout<<"Enter defective row and column"<<endl;
+	cin>>a>>b;
+	// Here tile can not be placed
+	arr[a][b] = -1;
+	tile(size_of_grid, 0, 0);
+	// The grid is
+	for (int i = 0; i < size_of_grid; i++) {
+		for (int j = 0; j < size_of_grid; j++)
+			cout << arr[i][j] << " \t";
+		cout << "\n";
+	}
 }
